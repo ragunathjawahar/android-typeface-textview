70600b1500e5599de50b591008d5d8dfdb655c94
Ragunath Jawahar R <ragunathjawahar@gmail.com>
2013-04-03 14:01:39 +0530

    Initial import.

---
+++ library/src/com/mobsandgeeks/ui/TypefaceTextView.java
@@ -0,0 +1,55 @@
+package com.mobsandgeeks.ui;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.mobsandgeeks.ui.R.styleable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TypefaceTextView extends TextView {
+
+    protected static Map<String, Typeface> mTypefaces;
+
+    public TypefaceTextView(final Context context) {
+        this(context, null);
+    }
+
+    public TypefaceTextView(final Context context, final AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TypefaceTextView(final Context context, final AttributeSet attrs, final int defStyle) {
+        super(context, attrs, defStyle);
+        if (mTypefaces == null) {
+            mTypefaces = new HashMap<String, Typeface>();
+        }
+
+        final TypedArray array = context.obtainStyledAttributes(attrs, styleable.TypefaceTextView);
+        if (array != null) {
+            final String typefaceAssetPath = array.getString(
+                    R.styleable.TypefaceTextView_customTypeface);
+
+            if (typefaceAssetPath != null) {
+                Typeface typeface = null;
+
+                if (mTypefaces.containsKey(typefaceAssetPath)) {
+                    typeface = mTypefaces.get(typefaceAssetPath);
+                } else {
+                    AssetManager assets = context.getAssets();
+                    typeface = Typeface.createFromAsset(assets, typefaceAssetPath);
+                    mTypefaces.put(typefaceAssetPath, typeface);
+                }
+
+                setTypeface(typeface);
+            }
+            array.recycle();
+        }
+    }
+
+}
---
+++ sample/src/com/mobsandgeeks/ui/demo/SampleActivity.java
@@ -0,0 +1,14 @@
+package com.mobsandgeeks.ui.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sample);
+    }
+
+}
